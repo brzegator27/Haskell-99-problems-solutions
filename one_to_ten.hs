b3e6a43c62db99191597270b0df3f1d9178d8336
Jakub Brzegowski <symfiz@gmail.com>
2016-02-21 17:00:32 +0100

    Solved problem 5th.

--- one_to_ten.hs
+++ one_to_ten.hs
@@ -28,3 +28,7 @@ elementAt' _ _ = error "Out of bound"
 myLength :: [a] -> Int
 myLength [] = 0
 myLength (_:xs) = 1 + myLength xs
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = (myReverse xs) ++ [x]
